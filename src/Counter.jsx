f49d42d7516c9a7d2900d05c77de6150f4e4e8da
Yash Kaskar <yash.kaskar99@gmail.com>
2023-09-08 06:40:28 +0530

    Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,18 @@
-export default function Counter(){
+import { useState } from "react"
+
+export default function Counter() {
+    const [num, setNum] = useState(0)
+    const changenumadd = () => {    
+        setNum(num + 1)
+    }
+    const changenumsub = () => {    
+        setNum(num - 1)
+    }
     return (    
             <div>   
-                Counting
+            <p>The count is : {num}</p>
+            <button onClick={changenumadd}>Increment</button>
+            <button onClick={changenumsub}>Decrement</button>
             </div>
         )
 }
