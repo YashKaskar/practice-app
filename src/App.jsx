1ea029b674a7731f17c3d8d4d55ce0138c9c2e1c
Yash Kaskar <yash.kaskar99@gmail.com>
2023-09-10 23:13:30 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Counter from './Counter';
+import Face from './Face';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Counter />
+      <Face />
     </>
   )
 }
---
+++ src/Face.jsx
@@ -0,0 +1,8 @@
+import "./toggler.css"
+export default function Face() {
+    return (    
+        <>
+        <p className="toggler">🙂</p>
+        </>
+    )
+}
