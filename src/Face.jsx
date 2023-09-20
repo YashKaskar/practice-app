c37184260d1d1232c3625b4f2c69cb7df8069c75
Yash Kaskar <yash.kaskar99@gmail.com>
2023-09-20 12:15:27 +0530

    face toggler

--- src/Face.jsx
+++ src/Face.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
 import "./toggler.css"
 export default function Face() {
+    const [isHappy, setIsHappy] = useState(true)
+    const Excited = () => setIsHappy(!isHappy)
+
+    
     return (    
         <>
-        <p className="toggler">🙂</p>
+            <p className="toggler" onClick={Excited}>
+            {isHappy ? "🤩" : "🙂"} 
+            </p>
         </>
     )
 }
+
