9c4a15140d9a982f189be2ddc6cf9333068b9930
Yash Kaskar <yash.kaskar99@gmail.com>
2023-09-20 12:29:30 +0530

    Heart emoji

--- src/Face.jsx
+++ src/Face.jsx
@@ -2,13 +2,22 @@ import { useState } from "react"
 import "./toggler.css"
 export default function Face() {
     const [isHappy, setIsHappy] = useState(true)
+
     const Excited = () => setIsHappy(!isHappy)
 
+    const [heart, setHeart] = useState(true)
+    const likeheart = () => setHeart(!heart)
     
     return (    
         <>
             <p className="toggler" onClick={Excited}>
-            {isHappy ? "🤩" : "🙂"} 
+                Click Me and get Excited <br />
+                {isHappy ? "🙂" : "🤩"} 
+                {isHappy ? "Happy" : "Excited"}
+            </p>
+            <p className="toggler" onClick={likeheart}>
+                {heart ? "❤️" : "💖"} <br />
+                {heart ? "Heart" : "Twinckle heart "}
             </p>
         </>
     )
