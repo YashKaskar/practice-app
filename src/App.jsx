87bca21bd71e96ebef11035be0c7112304b6036b
Yash Kaskar <yash.kaskar99@gmail.com>
2023-10-03 16:05:03 +0530

    Change color boxcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Counter from './Counter';
 import Face from './Face';
+import Colorbox from './Colorbox';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Counter />
       <Face />
+      <Colorbox />
     </>
   )
 }
